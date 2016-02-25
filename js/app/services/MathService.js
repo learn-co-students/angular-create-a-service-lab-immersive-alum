78f5d3c5c32696d184d701d5f26f18f5c4740685
ipc103 <iancandysemail@gmail.com>
2016-02-25 13:32:14 -0500

    remove solution

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,14 +1,1 @@
 // Your code here!
-function MathService(){
-  this.sum = function(arr){
-    var total = 0;
-    arr.forEach(function(number){
-      total += number;
-    })
-    return total;
-  }
-}
-
-angular
-  .module('app')
-  .service('MathService', MathService)
