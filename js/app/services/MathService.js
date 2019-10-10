ec24d9972a110404d55bc205784bcf690a760275
Rebecca Huseyin <rebecca_huseyin@outlook.com>
2019-10-10 14:24:53 +0000

    Done.

--- js/app/services/MathService.js
+++ js/app/services/MathService.js
@@ -1,1 +1,9 @@
-// Your code here!
+function MathService() {
+  this.sum = function(array){
+    array.reduce((a, b) => a + b, 0);
+  };
+}
+
+angular
+  .module('app')
+  .service('MathService', MathService);
